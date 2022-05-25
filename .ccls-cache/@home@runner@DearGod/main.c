3d6bc14c1cadbe8bdeedf18e461c23663e773ed5
Miguel Melnik <105880954+Hieki1@users.noreply.github.com>
2022-05-25 19:28:39 +0000

    final-c1

--- .ccls-cache/@home@runner@DearGod/main.c
+++ .ccls-cache/@home@runner@DearGod/main.c
@@ -1,36 +1,77 @@
+// Trabalho de Desenvolvimeto de Algoritmos - Jogo da Forca - 2022
+// Miguel Montenegro Melnik 11.119.035-1
+
+// Inlusão das bibliotecas utilizadas.
 #include <stdio.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 
+// Definição do struct dicionario possuindo dois vetores char, bem como das variáves que são desse tipo (item_cadastro, item_consulta, item_jogo, item_dicionario).
 struct dicionario{
 char palavra[46];
 char significado[200];
-}item_cadastro, item_consulta, item_jogo, item_dicionario;
+}item_cadastro, item_consulta, item_jogo, item_dicionario; 
 
-FILE *arquivo_dicionario; // arquivo global
+// Definição do arquivo_dicionario como variável global do tipo ponteiro FILE, para a utilização em diversas partes do código.
+FILE *arquivo_dicionario; 
 
+// Declaração das funções primárias do programa
+
+// Declaração da função tipo void responsável pelo cadastro de novas palavras no arquivo de dicionário do tipo texto.
 void cadastrar_palavra();
+
+// Declaração da função tipo void responsável pela consulta de palavras já cadastradas no arquivo de dicionário do tipo texto.
 void consultar_palavra();
+
+// Declaração da função tipo void responsável pelo início do jogo da forca com as palavras já cadastradas no arquivo de dicionário do tipo texto.
 void iniciar_jogo();
 
+// Declaração das funções secundárias do programa, respomsáveis por certas funções utilizadas dentro das funções primárias.
+
+// Declaração da função tipo int responsável pela verificação da existência, ou não, do arquivo de dicionário do tipo texto.
 int verificar_existencia_de_arquivo();
+
+// Declaração da função tipo int responsável pela verificação da existência, ou não, da palavra, endereçada pelo ponteiro char recebido, no arquivo de dicionário do tipo texto.
 int verificar_existencia_da_palavra(char *palavra_inserida);
+
+// Declaração da função tipo void responsável por tornar todos os caracteres, endereçados pelo ponteiro char recebido, minúsculos, evitando, assim, com que palavras iguais, como "MACACO" e "macaco", sejam indentificadas pelo programa como diferentes. 
 void normalizar_palavra(char *palavra);
+
+// Declaração da função tipo void responsável por exibir uma mensagem solicitando o pressionamento de qualquer caractere, para continuar a execução do programa e, com isso, graças a suas posições no código, retornar para o menu principal.
 void retornar_menu();
+
+// Declaração da função tipo void responsável por exbir o desenho da forca de acordo com o número de erros recebidos.
 void exibir_forca(int erros);
 
+// Declaração das funções terciárias, responsáveis pela estilização do texto exibido.
 
+// Declaração da função tipo void responsável por pintar o conteúdo endereçado pelo ponteiro char recebido, da cor verde e em negrito.
 void string_verde_negrito(char *string);
+
+// Declaração da função tipo void responsável por pintar o conteúdo endereçado pelo ponteiro char recebido, da cor verde, em negrito e sublinhado.
 void string_verde_negrito_sublinhado(char *string);
+
+// Declaração da função tipo void responsável por pintar o conteúdo endereçado pelo ponteiro char recebido, da cor vermelha e em negrito.
 void string_vermelha_negrito(char *string);
+
+// Declaração da função tipo void responsável por pintar o conteúdo endereçado pelo ponteiro char recebido, da cor vermelha, em negrito e sublinhado.
 void string_vermelha_negrito_sublinhado(char *string);
 
+// Definição da função do tipo void principal responsável por: exibir as opções do programa para o usuário, receber sua entrada e, de acordo com essa, chamar as funções primárias.
 int main(void) {
-  int op;
+  
+  // Declaração da variável local do tipo inteiro para receber a a opção selecionada pelo usuário de acordo com o menu.
+  int op; 
+  
+  // Início de um processo de loop que só termina com a inserção da opção 4, solicitando o encerramento da execução do programa.
   do{ 
-  system("clear");
+    
+  // Limpeza de qualquer informação exibida na tela.
+  system("clear"); 
+    
+  // Exibe o menu com as opções disponíveis para seleção.
   printf("========================================================");
   printf("\nMenu de Opções Principal\n");
   printf("========================================================");
@@ -40,23 +81,36 @@ int main(void) {
   printf("\n4 - Encerrar o Programa\n");
   printf("========================================================"); 
   printf("\nDigite o Número da Opção Desejada: ");
-  scanf("%d%*c", &op);
+    
+  // Recebe o input do tipo int do usuário, descartando o caractere criado com o enter, que poderia preencher o buffer.
+  scanf("%d%*c", &op); 
+    
+    // Chama, de acordo com a variável inteira recebida, uma das funções primárias.
     switch(op){
+
+      // Chama a função de cadastro de nova palavra e depois encerra a interação do loop, fazendo com que se recomece o ciclo, com uma nova interação.
       case 1: 
         cadastrar_palavra();
         break;
+
+      // Chama a função de consulta de nova palavra e depois encerra a interação do loop, fazendo com que se recomece o ciclo, com uma nova interação.
       case 2: 
         consultar_palavra();
         break;
+      
+       // Chama a função de jogo da forca e depois encerra a interação, fazendo com que se recomece o ciclo, com uma nova interação.
       case 3: 
         iniciar_jogo(); 
         break;
+
+      // Chama a função que encerra a interação e também o loop, solicitando, assim, o encerramento da execução do programa.
       case 4:
         break;
     }
   }while(op!=4);
 }
- 
+
+
 void cadastrar_palavra(){
   if (verificar_existencia_de_arquivo()==0){
     arquivo_dicionario=fopen("dicionario.txt","w");
@@ -92,15 +146,16 @@ void cadastrar_palavra(){
 }
 
 void consultar_palavra(){
-  system("clear");
-  printf("========================================================");
-  printf("\nConsulta de Palavra\n");
-  printf("========================================================");
+  
   if (verificar_existencia_de_arquivo()==0){
-    string_vermelha_negrito("\nErro! O Dicionário Não Contém Nenhuma Palavra.");
+    string_vermelha_negrito("\nErro! O Dicionário Não Contém Nenhuma Palavra.\n");
     retornar_menu();
   }
   else{
+    system("clear");
+    printf("========================================================");
+    printf("\nConsulta de Palavra\n");
+    printf("========================================================");
     printf("\nDigite a Palavra para Consulta: ");
     fgets(item_consulta.palavra,sizeof(item_consulta.palavra),stdin);
     item_consulta.palavra[strcspn(item_consulta.palavra,"\n")] = 0;
@@ -127,7 +182,7 @@ void consultar_palavra(){
 void iniciar_jogo(){
   int linha_palavra_sorteada, total_palavras_arquivo_dicionario=0;
   if (verificar_existencia_de_arquivo()==0){
-    string_vermelha_negrito("Erro! O Dicionário Não Contém Nenhuma Palavra.");
+    string_vermelha_negrito("\nErro! O Dicionário Não Contém Nenhuma Palavra.\n");
     retornar_menu();
   }
   else{
@@ -149,7 +204,8 @@ void iniciar_jogo(){
       item_jogo_palavra_oculta[i]='_';
     }
 
-    char letras_tentadas[26]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};      char letra;
+    char letras_tentadas[26]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};      
+    char letra;
     int erros=0, acertos=0, sinaliza_erros, contador=0;
     while(1){
       exibir_forca(erros);
@@ -193,7 +249,7 @@ void iniciar_jogo(){
         if(erros==6){
           exibir_forca(erros);
           for (int i=0;i<strlen(item_jogo.palavra);i++){
-          printf("\t%c",item_jogo_palavra_oculta[i]);
+            printf("\t%c",item_jogo_palavra_oculta[i]);
           }
           printf("\n"); 
           printf("\n========================================================\n");
@@ -208,7 +264,7 @@ void iniciar_jogo(){
           string_vermelha_negrito("\nA Palavra Secreta Era: ");
           string_vermelha_negrito_sublinhado(item_jogo.palavra);
           string_vermelha_negrito(" Que Significa: ");
-          string_vermelha_negrito_sublinhado(item_jogo.significado);
+          string_vermelha_negrito(item_jogo.significado);
           printf("\n");
           getchar();
           retornar_menu();
@@ -217,20 +273,22 @@ void iniciar_jogo(){
         else if (acertos==strlen(item_jogo.palavra)){
           exibir_forca(erros);
           for (int i=0;i<strlen(item_jogo.palavra);i++){
-          printf("%c\t",item_jogo_palavra_oculta[i]);
+            printf("\t%c",item_jogo_palavra_oculta[i]);
           }
           printf("\n"); 
           printf("\n========================================================\n");
           printf("Letras Já Tentadas:");
-          for (int i=0;i<strlen(letras_tentadas);i++){
-          printf("%c\t",letras_tentadas[i]);
+          for (int i=0;i<26;i++){
+             if(letras_tentadas[i]!='0'){
+              printf("%c ",letras_tentadas[i]);
+            }
           }
           printf("\n");
           string_verde_negrito("\nVocê Ganhou! O Boneco Perin Não Foi Enforcado!\n");
           string_verde_negrito("\nA Palavra Secreta Era: ");
           string_verde_negrito_sublinhado(item_jogo.palavra);
           string_verde_negrito(" Que Significa: ");
-          string_verde_negrito_sublinhado(item_jogo.significado);
+          string_verde_negrito(item_jogo.significado);
           printf("\n");
           getchar();
           retornar_menu();
